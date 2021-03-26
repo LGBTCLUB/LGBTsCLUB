986853dc4dc39d219a9712df6ae8387774685bd9
shrashtiiee <shrashti.pal.sbg@gmail.com>
2021-03-26 17:54:46 +0530

    initial commit

---
+++ app/src/main/java/com/lgbt/LGBTsCLUB/activity/VerificationActivity.java
@@ -0,0 +1,64 @@
+package com.lgbt.LGBTsCLUB.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.lgbt.LGBTsCLUB.R;
+
+public class VerificationActivity extends AppCompatActivity {
+
+   private TextInputEditText editTextCountryCode, editTextPhone;
+
+    Button btnverify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        editTextCountryCode = findViewById(R.id.editTextCountryCode);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        btnverify = findViewById(R.id.btn_verify);
+
+        btnverify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = editTextCountryCode.getText().toString().trim();
+                String number = editTextPhone.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editTextPhone.setError("Valid number is required");
+                    editTextPhone.requestFocus();
+                    return;
+                }
+
+                String phoneNumber = code + number;
+
+                Intent intent = new Intent(VerificationActivity.this, CodeVerficationActivity.class);
+                intent.putExtra("phoneNumber", phoneNumber);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, VerificationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
+    }
+}
